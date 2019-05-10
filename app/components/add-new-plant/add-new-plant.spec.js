b96a425b4f301b31f4d3d46147c942d9499e7fcc
Oleksandr Ivanov6 <Oleksandr_Ivanov6@epam.com>
2019-05-10 13:10:36 +0300

    unit tests of add-new-plant component

--- app/components/add-new-plant/add-new-plant.spec.js
+++ app/components/add-new-plant/add-new-plant.spec.js
@@ -3,65 +3,89 @@ describe("Testing addPlantCtrl", function(){
   var ctrl;
   var $mdDialog;
   var flowersFactory;
+  var inputData;
 
-  // module("wateringTracker");
-  // inject(function($rootScope, $controller, _$mdDialog_) {
-  //   $mdDialog = _$mdDialog_;
-  //   scope = $rootScope.$new();
-  //   $controller("addPlantCtrl", {$scope: scope});
-  // });
-  beforeEach(module("wateringTracker"));
-  beforeEach(inject(function($rootScope, $controller, _$mdDialog_, _flowersFactory_) {
-    scope = $rootScope.$new();
-    $mdDialog = _$mdDialog_;
-    flowersFactory = _flowersFactory_;
-    ctrl = $controller("addPlantCtrl", {$scope: scope});
-  }))
+  function getAddNewPlantDialog(item) {
+    inject(function($rootScope, $controller, _$mdDialog_, _flowersFactory_) {
+      scope = $rootScope.$new();
+      scope.plant = item;
+      $mdDialog = _$mdDialog_;
+      flowersFactory = _flowersFactory_;
+      ctrl = $controller("addPlantCtrl", {$scope: scope});
+    });
 
-  // it("Should create hide(), cancel(), answer() and addPlant() functions", function() {
-  //   expect(scope.hide).toBeDefined();
-  //   expect(scope.cancel).toBeDefined();
-  //   expect(scope.answer).toBeDefined();
-  //   expect(scope.addPlant).toBeDefined();
-  // });
+    spyOn(flowersFactory.database, "$add").and.callFake(function() {
+      return Promise.resolve();
+    });
+  };
 
-  // it("Should invoke $mdDialog.hide() during hide() call", function() {
-  //   spyOn($mdDialog, "hide");
-  //   scope.hide();
-  //   expect($mdDialog.hide).toHaveBeenCalled();
-  // });
-
-  // it("Should invoke $mdDialog.cancel() during cancel() call", function() {
-  //   spyOn($mdDialog, "cancel");
-  //   scope.cancel();
-  //   expect($mdDialog.cancel).toHaveBeenCalled();
-  // });
-  // TODO
-  //the above tests do not work due to the following error.
-  // [DEFAULT]: Firebase: Firebase App named '[DEFAULT]' already exists (app/duplicate-app).
-
-  it("Should invoke $mdDialog.hide() during answer() call", function() {
-    spyOn($mdDialog, "hide");
-    scope.answer();
-    expect($mdDialog.hide).toHaveBeenCalled();
+  beforeEach(function() {
+    angular.mock.module("wateringTracker");
+    MockFirebase.override();
   });
 
-  // TODO
-  // uncompleted test
-  // it("Should pass a new plant to firebase service", function() {
-  //   spyOn(flowersFactory.database, "$add");
-  //   var newItem = {
-  //     name: "a new plant name",
-  //     img: "http://someurl.com",
-  //     watering_inter: 5
-  //   };
-  //
-  //   scope.item = newItem;
-  //   console.log(ctrl);
-  //   debugger;
-  //   scope.addPlant();
-  //   console.log(flowersFactory.database.$add.calls());
-  //   expect(flowersFactory.database.$add).toHaveBeenCalled();
-  // });
+  describe("Initialization", function(){
 
+    beforeEach(function() {
+      getAddNewPlantDialog({});
+    });
+
+    it("Should create hide(), cancel(), answer() and addPlant() functions", function() {
+      expect(scope.hide).toBeDefined();
+      expect(scope.cancel).toBeDefined();
+      expect(scope.answer).toBeDefined();
+      expect(scope.addPlant).toBeDefined();
+    });
+
+    it("Should invoke $mdDialog.hide() during hide() call", function() {
+      spyOn($mdDialog, "hide");
+      scope.hide();
+      expect($mdDialog.hide).toHaveBeenCalled();
+    });
+
+    it("Should invoke $mdDialog.cancel() during cancel() call", function() {
+      spyOn($mdDialog, "cancel");
+      scope.cancel();
+      expect($mdDialog.cancel).toHaveBeenCalled();
+    });
+
+    it("Should invoke $mdDialog.hide() during answer() call", function() {
+      spyOn($mdDialog, "hide");
+      scope.answer();
+      expect($mdDialog.hide).toHaveBeenCalled();
+    });
+
+    it("A new plant should be in Watered state", function() {
+      scope.addPlant();
+      expect(scope.plant.status).toEqual("watered");
+    })
+
+    it("Should call flowersFactory.database.$add method", function() {
+      scope.addPlant();
+      expect(flowersFactory.database.$add).toHaveBeenCalledWith(scope.plant);
+    })
+  });
+
+  describe("Input Data", function() {
+
+    beforeEach(function() {
+      inputData = {
+        watering_interval: 5,
+        name: "testName",
+        img: "http://test"
+      };
+      getAddNewPlantDialog(Object.assign({}, inputData));
+    })
+
+    it("watering_interval should be sent to DB in milliseconds", function() {
+      scope.addPlant();
+      expect(scope.plant.watering_interval).toEqual(inputData.watering_interval * 60000);
+    })
+
+    it("img URL should start with 'http'", function() {
+      scope.addPlant();
+      expect(scope.plant.img.startsWith("http")).toEqual(true);
+    })
+
+  })
 })
